76b34ff79897bbd6d13ad523380f5e0b2a3039e1
DoanGiaHuy0301 <huydg0301@gmail.com>
2023-10-14 22:19:13 +0700

    add chat for teacher

--- src/components/TheMenu.vue
+++ src/components/TheMenu.vue
@@ -1,52 +1,58 @@
 <template>
-  <a-menu v-model:openKeys="openKeys" v-model:selectedKeys="selectedKeys" mode="inline">
+  <a-menu
+    v-model:openKeys="openKeys"
+    v-model:selectedKeys="selectedKeys"
+    mode="inline"
+  >
     <a-menu-item key="teacher-studentScore">
-      <router-link :to="{ name: 'teacher-studentScore' }" class="router-link-hover">
+      <router-link
+        :to="{ name: 'teacher-studentScore' }"
+        class="router-link-hover"
+      >
         <i class="fa-solid fa-pen"></i>
-        <span class="sider">
-        Nhập điểm
-        </span>
+        <span class="sider"> Nhập điểm </span>
       </router-link>
     </a-menu-item>
     <a-menu-item key="teacher-student">
       <router-link :to="{ name: 'teacher-student' }" class="router-link-hover">
         <i class="fa-solid fa-users"></i>
-        <span class="sider">
-        Sinh viên
-        </span>
+        <span class="sider"> Sinh viên </span>
       </router-link>
     </a-menu-item>
     <a-menu-item key="teacher-forum">
       <router-link :to="{ name: 'teacher-forum' }" class="router-link-hover">
         <i class="fa-solid fa-book"></i>
-        <span class="sider">
-          Diễn đàn
-        </span>
+        <span class="sider"> Diễn đàn </span>
       </router-link>
     </a-menu-item>
-
+    <a-menu-item key="teacher-chat">
+      <router-link :to="{ name: 'teacher-chat' }" class="router-link-hover">
+        <i class="fa-brands fa-rocketchat"></i>
+        <span class="sider"> Chat </span>
+      </router-link>
+    </a-menu-item>
     <a-menu-item key="teacher-classStudent">
-      <router-link :to="{ name: 'teacher-classStudent' }" class="router-link-hover">
+      <router-link
+        :to="{ name: 'teacher-classStudent' }"
+        class="router-link-hover"
+      >
         <i class="fa-solid fa-landmark"></i>
-        <span class="sider">
-          Lớp chủ nhiệm
-        </span>
+        <span class="sider"> Lớp chủ nhiệm </span>
       </router-link>
     </a-menu-item>
   </a-menu>
 </template>
 
 <script>
-  import { storeToRefs } from "pinia";
-  import { useMenu } from "../stores/use-menu.js";
-  import { defineComponent } from 'vue';
-  export default defineComponent({
-
-    setup() {
-      const store = useMenu();
-      return {
-        ...storeToRefs(store),
-      };
-    },
-  });
+import { storeToRefs } from "pinia";
+import { useMenu } from "../stores/use-menu.js";
+import { defineComponent } from "vue";
+export default defineComponent({
+  setup() {
+    const store = useMenu();
+    return {
+      ...storeToRefs(store),
+    };
+  },
+});
 </script>
