351f2e69fdf2efb5ad7195c4f54733bf2ece152c
Thanh <ngominhthanh852@gmail.com>
2023-10-22 17:57:11 +0700

    finish

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 # UniversitySB
-Hướng dẫn cài đặt hệ thống University Backend
-1. Giải nén project
-2. 
+- Hướng dẫn cài đặt hệ thống University Backend
+Bước 1. Tải về một trong hai IDE là IntelliJ hoặc Eclipse
+Bước 2. Giải nén project
+Bước 3. Mở project, chọn src -> main -> java -> com.nmt.universitysb 
+-> chuột phải vào UniversitySbApplicaiton -> Run UniversitySbApplicaiton
+Bước 4. Mở trình duyệt và gõ đường dẫn "http://localhost:8082"
+Bước 5. Đăng nhập với thông tin sau để test các chức năng
+tài khoản: giaovu
+mật khẩu: 123456
+
+- Hướng dẫn chạy hệ thống University Backend đã được đưa lên AWS (Amazon Web Serivce)
+Bước 1: Truy cập đường dẫn "http://universitysb-env.eba-nnscwuyq.ap-southeast-2.elasticbeanstalk.com/"
+Bước 2. Đăng nhập với thông tin sau để test các chức năng
+tài khoản: giaovu
+mật khẩu: 123456
--- src/main/java/com/nmt/universitysb/controller/ApiFirebaseController.java
+++ src/main/java/com/nmt/universitysb/controller/ApiFirebaseController.java
@@ -2,20 +2,17 @@ package com.nmt.universitysb.controller;
 
 import com.nmt.universitysb.dto.FirebaseProperties;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/firebase")
+@CrossOrigin
 public class ApiFirebaseController {
     @Autowired
     private FirebaseProperties firebaseProperties;
-
-    @Autowired
-    public ApiFirebaseController(FirebaseProperties firebaseProperties) {
-        this.firebaseProperties = firebaseProperties;
-    }
 
     @GetMapping("/config")
     public FirebaseProperties getFirebaseConfig() {
